1a94b43501ae8c5f856c69b018df4f05171fb7c7
Carlos8092 <157562812+Carlos8092@users.noreply.github.com>
2024-03-26 18:06:03 -0600

    Practica8: Hoja de acción y opciones correlices

--- miApp-2072114/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2072114/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-alumnos.component.scss'],
 })
 export class ListaAlumnosComponent  implements OnInit {
-  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
-
-  constructor() { }
+  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Barbara", "Georgina", "Kevin"];
+  
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit() {}
+  async Eliminar() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
 
+    await actionSheet.present();
+  }
 }
